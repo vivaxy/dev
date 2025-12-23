37ba7829f26e5b38430bcdbf68f2f7508a45269b
vivaxy <xyxuye2007@126.com>
2025-12-23 15:32:23 +0800

    chore: unregister service worker

---
+++ clipboard/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  self.registration
+    .unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach((client) => client.navigate(client.url));
+    });
+});
